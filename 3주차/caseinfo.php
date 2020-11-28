6365f452a77907beed1ca031336af3e9449465f5
CheeseCake116 <33691228+CheeseCake116@users.noreply.github.com>
2020-11-28 15:41:21 +0900

    Update caseinfo.php

--- 3주차/caseinfo.php
+++ 3주차/caseinfo.php
@@ -1,3 +1,6 @@
+<head>
+    <title> K_COVID19 TEAM6 </title>
+</head>
 <?php
     $link = mysqli_connect("localhost","userid","password", "k_covid19");
     if( $link === false )
@@ -12,13 +15,22 @@
         border: 1px solid #444444;
         border-collapse: collapse;
     }
-    th, td {
+
+    th,
+    td {
         border: 1px solid #444444;
+    }
+
+    h1,
+    h2 {
+        text-align: center;
     }
 </style>
 <body>
-    <h1 style="text-align:center"> 데이터베이스 팀 프로젝트 3주차</h1>
-    <hr style = "border : 5px solid yellowgreen">
+    <h1> 데이타베이스 6조 </h1>
+    <h2>곽진욱, 곽승규, 이주형</h2>
+    <hr style="border : 5px solid yellowgreen">
+    <h3> Select number of Confirmed.</h3>
     <?php
         $sql="select count(*) as num from caseinfo";
         $result = mysqli_query($link, $sql);
