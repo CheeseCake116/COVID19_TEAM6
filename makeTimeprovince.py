08118ab514b30101d4bc7a78347c5cc837765c34
CheeseCake116 <33691228+CheeseCake116@users.noreply.github.com>
2020-11-29 11:24:53 +0900

    주석 추가

--- makeTimeprovince.py
+++ makeTimeprovince.py
@@ -15,23 +15,19 @@ def main(argv):
 
     # Connection 으로부터 Cursor 생성
     cursor = conn.cursor()
-
     data = pd.read_csv('K_COVID19.csv')
 
-
-
-    # Using Hashing
-    # get province list from "K_COVID19.csv"
-    # ['Seoul' 'Busan' 'Daegu' ... 'Gyeongsangnam-do' 'Jeju-do']
+    # "K_COVID19.csv"로부터 province 리스트를 가져온다
+    # province_list = ['Seoul' 'Busan' 'Daegu' ... 'Gyeongsangnam-do' 'Jeju-do']
     province_list = data['province'].unique()
 
-    # province_list에서 NULL값 제거. 지역이 NULL값인 행은 읽지 않게 됨
+    # province_list에서 NULL값 제거. 지역이 NULL값인 행은 읽지 않게 하기 위해
     if 'NULL' in province_list:
         province_list.remove('NULL')
 
-    # get (province, confirmed_date) from "K_COVID19.csv" and count
-    # {날짜 : {지역 : 누적 수}} 로 이중 딕셔너리 사용
-    # {'2020-01-23': {'Seoul': 1}, '2020-01-30': {'Seoul': 3, 'Jeollabuk-do': 1}, ... }
+    # "K_COVID19.csv"에서 (province, confirmed_date) 튜플을 가져와 누적 환자 수를 계산해둔다
+    # {날짜 : {지역1 : 누적 수, 지역2 : 누적 수, ...}} 로 이중 딕셔너리 사용
+    # confirmed_date = {'2020-01-23': {'Seoul': 1}, '2020-01-30': {'Seoul': 3, 'Jeollabuk-do': 1}, ... }
     confirmed_date = data[['province', 'confirmed_date']]
     cdate_dic = {}
     for index, (province, date) in confirmed_date.iterrows():
@@ -44,12 +40,9 @@ def main(argv):
             temp = {province : 1}
             cdate_dic[date] = temp
 
-    # get (province, released_date) from "K_COVID19.csv" and count
-    # {'2020-01-23': {'Seoul': 1}, '2020-01-30': {'Seoul': 3, 'Jeollabuk-do': 1}, ... }
+    #released_date와 deceased_date도 마찬가지
     released_date = data[['province', 'released_date']]
     rdate_dic = {}
-
-
     for index, (province, date) in released_date.iterrows():
         if date in rdate_dic.keys():
             if province in rdate_dic[date].keys():
@@ -60,8 +53,6 @@ def main(argv):
             temp = {province : 1}
             rdate_dic[date] = temp
 
-    # get (province, deceased_date) from "K_COVID19.csv" and count
-    # {'2020-01-23': {'Seoul': 1}, '2020-01-30': {'Seoul': 3, 'Jeollabuk-do': 1}, ... }
     deceased_date = data[['province', 'deceased_date']]
     ddate_dic = {}
     for index, (province, date) in deceased_date.iterrows():
@@ -75,20 +66,19 @@ def main(argv):
             ddate_dic[date] = temp
 
 
-
-
     # 중복된 case 제거를 위해 checking list & variable
     date = []
+
+    # 누적량을 지역별로 계산하기 위해 각 지역을 key로 가지는 dictionary 사용
     total_confirmed = {}
     total_released = {}
     total_deceased = {}
-
-    # 지역별 누적을 위해 각 지역을 key로 가지는 dictionary 사용
     for province in province_list:
         total_confirmed[province] = 0
         total_released[province] = 0
         total_deceased[province] = 0
 
+    #1/20~6/30까지 날짜정보를 받기 위해 additional_Timeinfo.csv 파일을 open
     with open("addtional_Timeinfo.csv", 'r') as file:
         file_read = csv.reader(file)
 
@@ -118,11 +108,11 @@ def main(argv):
             else:
                 line[col_list['date']] = line[col_list['date']].strip()
 
-            # 지역별
+            # line에 들어온 현재 날짜에 있었던 각 지역별 변동사항을 얻기 위해 지역별 for문을 돌린다
             for province in province_list:
                 sql_data = []
-                sql_data.append(line[col_list['date']]) # 'date' 값 삽입
-                sql_data.append(province) # 'province' 값 삽입
+                sql_data.append(line[col_list['date']]) # sql에 'date' 값 삽입
+                sql_data.append(province) # sql에 'province' 값 삽입
 
                 if line[col_list['date']] in cdate_dic.keys():
                     if province in cdate_dic[line[col_list['date']]]:
